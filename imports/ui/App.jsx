f62bb5e91b2f0e9842cc54e41e2f68e7baf4f5c4
Srinivass Dhulasiraman <srinivass.dhulasiraman@gov.bc.ca>
2026-01-19 12:49:35 -0800

    Implement hide completed filter

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTracker, useSubscribe } from "meteor/react-meteor-data";
 import { Task } from "./Task.jsx";
 import { TasksCollection } from "../api/TasksCollection.js";
 import { TaskForm } from "./TaskForm.jsx";
 
 export const App = () => {
+  const [hideCompleted, setHideCompleted] = useState(false);
+
   const isLoading = useSubscribe("tasks");
 
   const tasks = useTracker(() =>
-    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch(),
+    TasksCollection.find(hideCompleted ? { isChecked: { $ne: true } } : {}, {
+      sort: { createdAt: -1 },
+    }).fetch(),
   );
+
+  const pendingTasksCount = useTracker(() =>
+    TasksCollection.find({ isChecked: { $ne: true } }).count(),
+  );
+
+  const pendingTasksTitle = pendingTasksCount ? `(${pendingTasksCount})` : "";
 
   const handleToggleChecked = ({ _id, isChecked }) => {
     Meteor.callAsync("tasks.toggleChecked", { _id, isChecked });
@@ -28,12 +38,17 @@ export const App = () => {
       <header>
         <div className="app-bar">
           <div className="app-header">
-            <h1>Meteor To Do</h1>
+            <h1>Meteor To Do {pendingTasksTitle}</h1>
           </div>
         </div>
       </header>
       <div className="main">
         <TaskForm />
+        <div className="filter">
+          <button onClick={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? "Show All" : "Hide Completed"}
+          </button>
+        </div>
         <ul className="tasks">
           {tasks.map((task) => (
             <Task
